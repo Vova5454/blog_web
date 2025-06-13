44cf754cd29ddb8333f0c143fbf43fa0c2955c71
Vova Ushakov <vova.u.new@gmail.com>
2025-06-13 18:14:29 +0500

    fixing more mistakes

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -5,29 +5,35 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField("Название", max_length=200)
-    slug = models.SlugField("slug", unique=True)
-    description = models.TextField("Описание")
+    title = models.CharField(verbose_name="Название", max_length=200)
+    slug = models.SlugField(verbose_name="Читабельный URL", unique=True)
+    description = models.TextField(verbose_name="Описание")
 
     def __str__(self):
         return self.title
+    
+    class Meta:
+        verbose_name = "Группа"
+        verbose_name_plural = "Группы"
 
 
 class Post(models.Model):
-    text = models.TextField("Текст")
-    pub_date = models.DateTimeField("Время выпуска")
-    author = models.ForeignKey("Пользователь", 
-                               User,
+    text = models.TextField(verbose_name="Текст")
+    pub_date = models.DateTimeField(verbose_name="Дата публикации")
+    author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
-                               related_name="posts")
-    group = models.ForeignKey("Группа", 
-                              Group,
+                               related_name="posts",
+                               verbose_name="Автор")
+    group = models.ForeignKey(Group,
                               on_delete=models.SET_NULL,
                               blank=True, null=True,
-                              related_name="posts")
+                              related_name="posts",
+                              verbose_name="Группа")
 
     def __str__(self):
         return self.text[:15]
 
     class Meta:
         ordering = ("-pub_date",)
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
