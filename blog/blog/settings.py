1a0d5defc39261343614647a068dcbf702c24ed1
Vova Ushakov <vova.u.new@gmail.com>
2025-06-13 16:55:01 +0500

    fixing mistakes

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -72,7 +72,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,26 +1,27 @@
-from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.db import models
 
 User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField("Название", max_length=200)
+    slug = models.SlugField("slug", unique=True)
+    description = models.TextField("Описание")
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField()
-    author = models.ForeignKey(User,
+    text = models.TextField("Текст")
+    pub_date = models.DateTimeField("Время выпуска")
+    author = models.ForeignKey("Пользователь", 
+                               User,
                                on_delete=models.CASCADE,
                                related_name="posts")
-    group = models.ForeignKey(Group,
+    group = models.ForeignKey("Группа", 
+                              Group,
                               on_delete=models.SET_NULL,
                               blank=True, null=True,
                               related_name="posts")
